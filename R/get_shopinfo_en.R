35ae34ba0fb540426079e4a29db5b830b5f6f894
Guangming Lang <rigor6vigor@gmail.com>
2017-05-03 22:28:18 -0400

    add get_shopinfo_en() and update get_shopinfo_ja()

---
+++ R/get_shopinfo_en.R
@@ -0,0 +1,126 @@
+#' @title Scrape restaurant information (in English) from a given Tabelog URL.
+#'
+#' @param shopURL string, URL of restaurant, for example,
+#'                "https://tabelog.com/en/kyoto/A2601/A260301/26002222/".
+#' @return a data frame of restaurant info in English.
+#'
+#' @importFrom dplyr "%>%"
+#'
+#' @export
+
+get_shopinfo_en = function(shopURL) {
+        # shopURL = "https://tabelog.com/en/kyoto/A2601/A260301/26006250/"
+        request = httr::RETRY("GET", url = shopURL)
+        check_request(request)
+        ids = xml2::read_html(request)
+
+        # pause a few seconds
+        Sys.sleep(2)
+
+        # extract ratings
+        ratings = suppressWarnings(
+                rvest::html_nodes(ids, ".rd-header__rst-rate .c-rating__val") %>%
+                        rvest::html_text() %>% stringr::str_trim() %>%
+                        as.numeric()
+                )
+        rating = ratings[1]
+        rating_dinner = ratings[2]
+        rating_lunch = ratings[3]
+
+        # extract number of reviews
+        reviews = suppressWarnings(
+                rvest::html_nodes(ids, ".rd-header__rst-reviews-target") %>%
+                        rvest::html_text() %>%
+                        gsub(pattern="\n| |reviews", replace="") %>%
+                        as.integer()
+                )
+
+        # extract prices
+        prices = rvest::html_nodes(ids, ".rd-header__info-table .c-rating__val") %>%
+                rvest::html_text()
+        price_dinner = prices[1]
+        price_lunch = ifelse(length(prices)==2, prices[2], NA_character_)
+
+        # extract detailed info table
+        tbl = rvest::html_nodes(ids, ".rd-detail-info") %>% rvest::html_text()
+        basic = tbl[1]
+        seats = tbl[2]
+        menu  = tbl[3]
+        other = tbl[4]
+
+        # extract basic shop info
+        shop_name = gsub(".*Restaurant name(.*)Categories.*", "\\1", basic) %>%
+                stringr::str_trim() %>% gsub(pattern = "\n *", replacement=" ")
+        cuisine = gsub(".*Categories(.*)TEL.*", "\\1", basic) %>%
+                stringr::str_trim()
+        tel = gsub(".*TEL/reservation(.*)Addresses.*", "\\1", basic) %>%
+                stringr::str_trim() %>%
+                stringr::str_extract(pattern="[0-9]+-[0-9]+-[0-9]+")
+        address = gsub(".*Addresses(.*)Show larger map.*", "\\1", basic) %>%
+                stringr::str_trim()
+        address_en = gsub("(.*[a-zA-Z]+).*", "\\1", address)
+        address_ja = gsub("\n| ", "", gsub(".*[a-zA-Z]+", "", address))
+        address = paste(address_en, paste0("(", address_ja, ")"))
+        nearby = gsub(".*Transportation(.*)Operating Hours.*", "\\1", basic) %>%
+                stringr::str_trim() %>%
+                stringr::str_extract(pattern = "[0-9a-zA-Z| ]+") %>%
+                stringr::str_trim() %>% gsub(pattern = ".*from ", replace="")
+        hours = gsub(".*Operating Hours(.*)Shop holidays.*", "\\1", basic) %>%
+                gsub(pattern = "□■.*", replace = "") %>%
+                stringr::str_trim()
+        hours = paste(substr(hours, 1, 11), substr(hours, 12, nchar(hours)),
+                      sep=" | ")
+        cards = gsub(".*Cards(.*)", "\\1", basic) %>%
+                gsub(pattern = ".*\\(|\\).*", replace="")
+
+        # extract seats info
+        private = gsub(".*Private dining rooms(.*)Private use.*", "\\1", seats) %>%
+                stringr::str_trim()
+        private = gsub(".*Private dining rooms(.*)Private use.*", "\\1", seats) %>%
+                stringr::str_trim()
+        smoking = gsub(".*Non-smoking/smoking(.*)Parking lot.*", "\\1", seats) %>%
+                stringr::str_trim() %>% gsub(pattern=" establishment", replace="")
+        parking = gsub(".*Parking lot(.*)Space/facilities.*", "\\1", seats) %>%
+                gsub(pattern="[^a-zA-Z]", replace="")
+
+        # extract other info
+        occasion = gsub(".*Occasion(.*)Location.*", "\\1", other) %>%
+                stringr::str_trim() %>% gsub(pattern=" *\n.*", replace="")
+        website = gsub(".*The homepage(.*)The opening day.*", "\\1", other) %>%
+                stringr::str_trim()
+        open_date = gsub(".*The opening day(.*)First reviewer.*", "\\1", other) %>%
+                stringr::str_trim()
+
+        # fill in NA if info not available
+        if (length(shop_name)==0) shop_name = NA_character_
+        if (length(rating)==0) rating = NA_real_
+        if (length(rating_dinner)==0) rating_dinner = NA_real_
+        if (length(rating_lunch)==0) rating_lunch = NA_real_
+        if (length(reviews)==0) reviews = NA_integer_
+        if (length(price_dinner)==0) price_dinner = NA_character_
+        if (length(price_lunch)==0) price_lunch = NA_character_
+        if (length(tel)==0) tel = NA_character_
+        if (length(address)==0) address = NA_character_
+        if (length(cuisine)==0) cuisine = NA_character_
+        if (length(cards)==0) cards = NA_character_
+        if (length(private)==0) private = NA_character_
+        if (length(smoking)==0) smoking = NA_character_
+        if (length(parking)==0) parking = NA_character_
+        if (length(occasion)==0) occasion = NA_character_
+        if (length(website)==0) website = NA_character_
+        if (length(open_date)==0) open_date = NA_character_
+        if (length(nearby)==0) nearby = NA_character_
+
+        # collect into a data frame and return
+        out = data.frame(shop_name, cuisine, rating_dinner, rating_lunch,
+                         reviews, price_dinner, price_lunch, occasion, nearby,
+                         hours, cards, private, smoking, parking, address, tel,
+                         open_date, website, shopURL, stringsAsFactors = F)
+        names(out) = c("Restaurant Name", "Cuisine", "Dinner Rating",
+                       "Lunch Rating", "Reviews", "Dinner Price",
+                       "Lunch Price", "Good for", "Nearest Station", "Hours",
+                       "Accept Credit Cards", "Private Room", "Smoking",
+                       "Parking", "Address", "Tel", "Opened Since",
+                       "Restaurant Website", "View on Tabelog")
+        out
+}
--- R/get_shopinfo_ja.R
+++ R/get_shopinfo_ja.R
@@ -1,11 +1,11 @@
 #' @title Scrape restaurant information (in Japanese) from a given Tabelog URL.
-#' 
-#' @param shopURL string, URL of restaurant, for example, 
+#'
+#' @param shopURL string, URL of restaurant, for example,
 #'                "https://tabelog.com/kyoto/A2601/A260301/26002222/".
-#' @return a data frame of restaurant info in Japanese. 
-#' 
+#' @return a data frame of restaurant info in Japanese.
+#'
 #' @importFrom dplyr "%>%"
-#' 
+#'
 #' @export
 
 get_shopinfo_ja = function(shopURL) {
@@ -16,104 +16,103 @@ get_shopinfo_ja = function(shopURL) {
 
         # pause a few seconds
         Sys.sleep(2)
-        
+
         # extract name
-        shop_name = rvest::html_nodes(ids, ".display-name") %>% 
+        shop_name = rvest::html_nodes(ids, ".display-name") %>%
                 rvest::html_text() %>% stringr::str_trim()
-        
+
         # extract ratings
         rating = suppressWarnings(
-                rvest::html_nodes(ids, ".rdheader-rating__score-val") %>% 
-                rvest::html_text() %>% stringr::str_trim() %>% as.numeric() 
+                rvest::html_nodes(ids, ".rdheader-rating__score-val") %>%
+                rvest::html_text() %>% stringr::str_trim() %>% as.numeric()
                 )
-        rating_dinner = 
-                rvest::html_nodes(ids, ".rdheader-rating__time-icon--dinner") %>% 
-                rvest::html_text() 
+        rating_dinner =
+                rvest::html_nodes(ids, ".rdheader-rating__time-icon--dinner") %>%
+                rvest::html_text()
         rating_dinner = suppressWarnings(
                 gsub("\n| |夜の点数：", "", rating_dinner) %>% as.numeric()
                 )
-        rating_lunch = 
-                rvest::html_nodes(ids, ".rdheader-rating__time-icon--lunch") %>% 
+        rating_lunch =
+                rvest::html_nodes(ids, ".rdheader-rating__time-icon--lunch") %>%
                 rvest::html_text()
         rating_lunch = suppressWarnings(
                 gsub("\n| |昼の点数：", "", rating_lunch) %>% as.numeric()
                 )
-        
+
         # extract number of reviews
-        reviews = rvest::html_nodes(ids, ".rdheader-rating__review-target") %>% 
+        reviews = rvest::html_nodes(ids, ".rdheader-rating__review-target") %>%
                 rvest::html_text()
         reviews = suppressWarnings(
-                gsub("\n| |口コミ|件", "", reviews) %>% as.integer() 
+                gsub("\n| |口コミ|件", "", reviews) %>% as.integer()
                 )
-        
+
         # extract prices
-        price_dinner = 
-                rvest::html_nodes(ids, ".rdheader-budget__icon--dinner") %>% 
+        price_dinner =
+                rvest::html_nodes(ids, ".rdheader-budget__icon--dinner") %>%
                 rvest::html_text()
-        price_dinner = 
-                gsub("￥", "¥", gsub("～", " - ", 
+        price_dinner =
+                gsub("￥", "¥", gsub("～", " - ",
                                     gsub("\n| |夜の予算", "", price_dinner)))
-        price_lunch = rvest::html_nodes(ids, ".rdheader-budget__icon--lunch") %>% 
+        price_lunch = rvest::html_nodes(ids, ".rdheader-budget__icon--lunch") %>%
                 rvest::html_text()
-        price_lunch = 
-                gsub("￥", "¥", gsub("～", " - ", 
+        price_lunch =
+                gsub("￥", "¥", gsub("～", " - ",
                                     gsub("\n| |昼の予算", "", price_lunch)))
-        
+
         # extract phone number
         tel = rvest::html_nodes(ids, ".visit-action__tel") %>% rvest::html_text()
-        
+
         # extract opened date and weekly closed days
-        birth_date = rvest::html_nodes(ids, ".rstinfo-opened-date") %>% 
+        birth_date = rvest::html_nodes(ids, ".rstinfo-opened-date") %>%
                 rvest::html_text()
-        when_closed = rvest::html_nodes(ids, ".rdheader-subinfo__closed-text") %>% 
+        when_closed = rvest::html_nodes(ids, ".rdheader-subinfo__closed-text") %>%
                 rvest::html_text() %>% stringr::str_trim()
-        
-        # extract address 
-        address = rvest::html_nodes(ids, ".rstinfo-table__address") %>% 
-                rvest::html_text()
-        address = stringr::str_trim(address)
-        
+
+        # extract address
+        address = rvest::html_nodes(ids, ".rstinfo-table__address") %>%
+                rvest::html_text() %>% stringr::str_trim()
+
         # # extract prefecture, city and area
         # loc = rvest::html_nodes(ids, ".listlink") %>% rvest::html_text()
         # prefecture = loc[1]
         # city = loc[2]
         # area = loc[3]
-        
+
         # extract cuisine
-        cuisine = rvest::html_nodes(ids, ".linktree__parent") %>% 
+        cuisine = rvest::html_nodes(ids, ".linktree__parent") %>%
                 rvest::html_text() %>% dplyr::last() %>% stringr::str_trim()
-        
+
         # extract table of other info
-        tbl = rvest::html_nodes(ids, ".rstinfo-table__table") %>% 
+        tbl = rvest::html_nodes(ids, ".rstinfo-table__table") %>%
                 rvest::html_text()
         elt1 = gsub("\n| ", "", tbl %>% dplyr::first())
         elt2 = gsub("\n| ", "", tbl %>% dplyr::nth(2))
         elt4 = gsub("\n| ", "", tbl %>% dplyr::nth(4))
-        
+
         # extract business hours
         hours = gsub("営業時間|定休日", "",
                      stringr::str_extract(elt1, "営業時間.*定休日"))
-                
+
         # extract credit cards
         cards = gsub("カード|サービス料・チャージ", "",
                      stringr::str_extract(elt1, "カード.*サービス料・チャージ"))
-        
+
         # extract private room availability
         private = gsub("個室|禁煙", "", stringr::str_extract(elt2, "個室.禁煙"))
-        
+
         # extract smoking allowed or not
-        smoking = gsub("禁煙・喫煙", "", 
+        smoking = gsub("禁煙・喫煙", "",
                        stringr::str_extract(elt2, "禁煙・喫煙.*煙"))
 
         # extract parking availability
         parking = gsub("駐車場|空間", "", stringr::str_extract(elt2, "駐車場.空間"))
 
         # extract appropriate for what occasion
-        all_occasions = 
+        all_occasions =
                 "家族・子供と|デート|女子会|合コン|大人数の宴会|接待|一人で入りやすい|知人・友人と"
         occasion = paste(stringr::str_extract_all(elt4, all_occasions)[[1]],
                          collapse = " | ")
-        
+
         # fill in NA if info not available
         if (length(shop_name)==0) shop_name = NA_character_
         if (length(rating)==0) rating = NA_real_
@@ -132,7 +131,7 @@ get_shopinfo_ja = function(shopURL) {
         if (length(smoking)==0) smoking = NA_character_
         if (length(parking)==0) parking = NA_character_
         if (length(occasion)==0) occasion = NA_character_
-        
+
         # collect into a data frame and return
         data.frame(
                 店名 = shop_name,
@@ -153,6 +152,7 @@ get_shopinfo_ja = function(shopURL) {
                 住所 = address,
                 TEL = tel,
                 オープン日 = birth_date,
+                URL = shopURL,
                 stringsAsFactors = F
         )
 }
